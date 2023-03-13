21bdbbd1ba370f82cc251801d111400c1263ad3e
Laurin Schwartze <victor.schwartze@gmail.com>
2023-03-13 11:58:04 +0100

    finish GUI

---
+++ src/solver/Cube.java
@@ -0,0 +1,5 @@
+package solver;
+
+class Cube {
+
+}
--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -1,8 +1,93 @@
 package solver;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
 
-class Solver {
+import javax.swing.*;
+
+/*Solver class. Constructs the GUI to enter scramble and display solution. 
+Contains necessary functions to check for validity of scramble and makes calls to cube object.*/
+
+class Solver implements ActionListener{
+	
+	JLabel label;
+	JButton button;
+	JTextField text;
+	static String scramble;
+	
+	public Solver(){
+		JFrame frame = new JFrame("scramble input");
+		text = new JTextField();
+		text.setEditable(true);
+		label = new JLabel("please input your scramble in standard WCA notation");
+		button = new JButton("solve!");
+		button.addActionListener(this);
+		frame.add(text, BorderLayout.CENTER);
+		frame.add(button, BorderLayout.SOUTH);
+		frame.add(label, BorderLayout.NORTH);
+		frame.setSize(500,100);
+		frame.setVisible(true);
+	}
+
+	@Override
+	//handles input using textfield and button
+	public void actionPerformed(ActionEvent e) {
+		scramble = null;
+		if(e.getSource() == this.button) {
+			scramble = this.text.getText();
+		}	
+		
+		begin_solve();
+	}
+	
+	public void begin_solve() {
+		while(scramble == null) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(checkScramble(scramble)) {
+			this.label.setText("calculating...");
+			this.button.setEnabled(false);
+		}
+		else {
+			this.label.setText("invalid scramble");
+		}
+		return;
+	}
+	
+	public static boolean checkScramble(String s) {
+		Set<Character> valid_first = Set.of('R', 'L', 'U', 'D', 'F', 'B');
+		Set<Character> valid_modifier = Set.of('2', '\'');
+		s = s.replaceAll("\\s", "");
+		int i = 0;
+		while(i<s.length()-1) {
+			if(valid_first.contains(s.charAt(i))) {
+				if(valid_modifier.contains(s.charAt(i+1))) {
+					i += 2;
+					continue;
+				}
+				else if(valid_first.contains(s.charAt(i+1))) {
+					i += 1;
+					continue;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
 	
 	public static void main(String args[]) {
-		
+		new Solver();
 	}
+	
 }
